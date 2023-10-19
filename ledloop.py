25cd7a2228db14cb9b89ecef67aaba6179775be3
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2023-10-20 00:26:17 +0200

    working on socket control

--- ledloop.py
+++ ledloop.py
@@ -10,46 +10,78 @@ import neopixel
 
 context = zmq.Context()
 
+# Global configuration
+LED_DATA_PIN = board.D10
+COUNT_LED = 80
+ORDER = neopixel.GRB
+BRIGHTNESS = 0.4
+PIXELS = neopixel.NeoPixel(LED_DATA_PIN, COUNT_LED, pixel_order=ORDER, brightness=BRIGHTNESS)
+
 def config_socket(socket):
     socket.connect("ipc:///tmp/ledSequence")
     socket.setsockopt_string(zmq.SUBSCRIBE, "ledsequence")
 
-def start_reading_socket(socket):
-    config_socket(socket)
+def config_thread(target_function, event):
+    target_args = [event]
+    thread = threading.Thread(target=target_function, args=(target_args))
+    thread.daemon = True  # Set the thread as a daemon so it exits when the main program exits
+    return thread
 
-    def listener_function():
-        while True:
-            message = socket.recv_string()
-            topic , value = message.split(' ')
-            print(f"topic: {topic} , value: {value}")
+def led_on(event):
+    print("led_on")
+    while True:
+        for p in range(0,COUNT_LED):
+            PIXELS[p] = (0, 40, 0)
+            PIXELS.show()
+            time.sleep(0.5)
+            if event.is_set():
+                break
+        if event.is_set():
+            break
+    event.clear()
+    return
 
-    subscriber_thread = threading.Thread(target=listener_function)
-    subscriber_thread.daemon = True  # Set the thread as a daemon so it exits when the main program exits
-    subscriber_thread.start()
+def led_off(event):
+    print("led_off")
+    PIXELS.fill((0,0,0))
+    PIXELS.show()
+    event.clear()
+    return
 
-def stop_reading_socket(socket):
-    ##socket.close()
-    pass
 
 def main():
 
-    LED_DATA_PIN = board.D10
-    COUNT_LED = 80
-    ORDER = neopixel.GRB
-    BRIGHTNESS = 0.4
-
-    #Socket creation
+    # Socket creation
     inputSocket = context.socket(zmq.SUB)
-    start_reading_socket(inputSocket)
+    config_socket(inputSocket)
 
-    pixels = neopixel.NeoPixel(LED_DATA_PIN, COUNT_LED, pixel_order=ORDER, brightness=BRIGHTNESS)
+    # Change sequence event (aka killing event)
+    change_sec = threading.Event()
 
+    leds_thread = config_thread(led_on, change_sec)
+    leds_thread.start()
+    print("hey dude")
     try:
         while True:
-            for p in range(0,COUNT_LED):
-                pixels[p] = (0, 40, 0)
-                pixels.show()
-                time.sleep(0.5)
+            print("something")
+            message = inputSocket.recv_string()
+            topic , value = message.split(' ')
+            print(f"topic: {topic} , value: {value}")
+
+            def kill_led_thread():
+                if leds_thread.is_alive():
+                    change_sec.set()
+                    leds_thread.join()
+
+            if topic == "ledsequence":
+                if value == "on":
+                    kill_led_thread()
+                    leds_thread = config_thread(led_on, change_sec)
+                    leds_thread.start()
+                elif value == "off":
+                    kill_led_thread()
+                    leds_thread = config_thread(led_off, change_sec)
+                    leds_thread.start()
 
     except KeyboardInterrupt:
         # Manually stop all active listener threads if you press Ctrl+C
