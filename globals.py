075d9cd38354c8fb9bb5c6c0be1e40ff53370460
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2025-10-17 11:01:06 +0200

    disable PIXELS_2 by default

--- globals.py
+++ globals.py
@@ -9,24 +9,24 @@ APP_PATH = os.path.dirname(os.path.realpath(__file__))
 
 # Global configuration
 LED_DATA_PIN = board.D21   # pin 40 on a raspi zero w
-LED_DATA_PIN2 = board.D18  # pin 12 on a raspi zero w
+#LED_DATA_PIN2 = board.D18  # pin 12 on a raspi zero w
 COUNT_LED = 18
 COUNT_LED2 = 24
 RGB = neopixel.GRB
 BRIGHTNESS = 0.4
 PIXELS = neopixel.NeoPixel(LED_DATA_PIN, COUNT_LED, pixel_order=RGB, brightness=BRIGHTNESS, auto_write=False)
-PIXELS_2 = neopixel.NeoPixel(LED_DATA_PIN2, COUNT_LED2, pixel_order=RGB, brightness=BRIGHTNESS, auto_write=False)
+#PIXELS_2 = neopixel.NeoPixel(LED_DATA_PIN2, COUNT_LED2, pixel_order=RGB, brightness=BRIGHTNESS, auto_write=False)
 
 tupla_PIXELS = ("PIXELS", PIXELS)
-tupla_PIXELS_2 = ("PIXELS_2", PIXELS_2)
+#tupla_PIXELS_2 = ("PIXELS_2", PIXELS_2)
 
 pixelSceneDict = {}
 pixelSceneDict["PIXELS"] = PIXELS
-pixelSceneDict["PIXELS_2"] = PIXELS_2
+#pixelSceneDict["PIXELS_2"] = PIXELS_2
 
 pixelSceneList = []
 pixelSceneList.append(PIXELS)
-pixelSceneList.append(PIXELS_2)
+#pixelSceneList.append(PIXELS_2)
 
 def update_all():
     for pixel in pixelSceneList:
